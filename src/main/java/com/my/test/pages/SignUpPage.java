dc4e6f3a85baa7a4ae6259f47990e104d975d0d9
Sunil singh <sghsunil@yahoo.com>
2025-09-16 16:08:39 -0400

    updated the code

--- src/main/java/com/my/test/pages/SignUpPage.java
+++ src/main/java/com/my/test/pages/SignUpPage.java
@@ -1,6 +1,10 @@
 package com.my.test.pages;
 
+import org.openqa.selenium.By;
+
 public class SignUpPage {
+	
+	By signup = By.id("sign up");
 	public SignUpPage() {
 		System.out.print("Signup Page");
 		
